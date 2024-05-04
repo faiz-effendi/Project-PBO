afa05ebf962bdc770c71a273c917b27532e7807e
abeaar <140606965+abeaar@users.noreply.github.com>
2024-05-04 23:40:22 +0700

    Update SegiTiga.java

--- src/benda_dua_dimensi/SegiTiga.java
+++ src/benda_dua_dimensi/SegiTiga.java
@@ -1,5 +1,21 @@
 package benda_dua_dimensi;
 
-public class SegiTiga{
-
+public class SegiTiga implements BendaDuaDimensi{
+    public float sisi;
+    public float luasSegitiga;
+    public float kelilingSegitiga;
+    
+    public SegiTiga(float sisi){
+        this.sisi = sisi;
+        luas();
+        keliling();
+    }
+    
+    public void luas(){
+        luasSegitiga = (float) ((Math.sqrt(3) / 4) * Math.pow(sisi, 2));
+    }
+    
+    public void keliling(){
+        kelilingSegitiga = sisi*3;
+    }
 }
