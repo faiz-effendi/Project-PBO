eb67399bdf7412f4bf1a412a054e4f31423c3b8d
ARiP001 <142637487+ARiP001@users.noreply.github.com>
2024-05-05 21:19:46 +0700

    Update Trapesium.java

--- src/benda_dua_dimensi/Trapesium.java
+++ src/benda_dua_dimensi/Trapesium.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package benda_dua_dimensi;
 
-/**
- *
- * @author ASUS
- */
-public class Trapesium {
+public class Trapesium implements BendaDuaDimensi{
+    public float sisiA;
+    public float sisiB;
+    public float tinggi;
+    public float luasTrapesium;
+    public float kelilingTrapesium;
     
+    public Trapesium(float sisiA, float sisiB, float tinggi){
+        this.sisiA = sisiA;
+        this.sisiB = sisiB;
+        this.tinggi = tinggi;
+        luas();
+        keliling();
+    }
+    
+    public void luas(){
+        luasTrapesium = (sisiA + sisiB)/2 * tinggi;
+    }
+    
+    public void keliling(){
+        float sisimiring;
+        sisimiring = (float) Math.sqrt(Math.pow((sisiB-sisiA), 2) + Math.pow(tinggi, 2));
+        kelilingTrapesium = 2*sisimiring + sisiA + sisiB;
+    }
 }
